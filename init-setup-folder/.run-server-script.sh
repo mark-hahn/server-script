f195fd04f6415d8d8f39014d5af84a61852fc6de
Mark Hahn <mark@hahnca.com>
2016-03-20 16:43:26 -0700

    added more init files

---
+++ init-setup-folder/.run-server-script.sh
@@ -0,0 +1,10 @@
+######################################################
+#  DO NOT EDIT.  THIS FILE IS AUTOMATICALLY CREATED  #
+######################################################
+
+# This file is created by the server-script package
+# it is run by node's exec command to copy changed files and run the remote script
+# if you know what you are doing, you can rename this and run it when you wish
+#
+# this file is kept out of the git repo so secrets aren't stored
+#
---
+++ init-setup-folder/build.sh
@@ -0,0 +1,6 @@
+
+# This script runs on the server
+# A server script file can have any name, set names in .server-script/server-setup.cson
+# replace the contents of this file with your server script or replace the file entirely.
+
+echo "Starting project build"
